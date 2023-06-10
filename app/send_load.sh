7abdf6d5ca6581a5753d481a14992073c714d10a
mehran <mhr.as1317@gmail.com>
2023-06-10 14:35:08 +0330

    Update help and usage for send_load.sh

--- app/send_load.sh
+++ app/send_load.sh
@@ -2,34 +2,43 @@
 
 # send_load.sh - A script for sending load to a cluster
 
-# Usage:
-#   send_load.sh [OPTIONS]
+Usage(){
+  cat << EOF
+  
+    send_load.sh [OPTIONS]
 
-# Options:
-#   -d, --default-file <file>   : Path to the default file. (Default: ./../conf/defaults.json)
-#   -i, --input-file <file>     : Path to the input file. (Default: ./../conf/benchmark.cfg)
-#   -s, --script-file <file>    : Path to the script file.
+Options:
+  -d, --default-file <file>   : Path to the default file. (Default: ./../conf/defaults.json)
+  -b, --benchmark-file <file>     : Path to the benchmark file. (Default: ./../conf/benchmark.cfg)
+  -s, --script-file <file>    : Path to the script file. (Default: ./pre_test_script.sh)
 
-# Description:
-#   This script sends load to a cluster based on the provided input and default files.
+Description:
+  This script sends load to a cluster based on the provided benchmark and default files.
+  Scirpt-file execute before every test.
 
-# Example usage:
-#   send_load.sh -d /path/to/defaults.json -i /path/to/benchmark.cfg -s /path/to/script.sh
-#   send_load.sh -s /path/to/script.sh    (uses default input and default files)
+Example usage:
+  send_load.sh -d /path/to/defaults.json -b /path/to/benchmark.cfg -s /path/to/script.sh
+  send_load.sh -s /path/to/script.sh    (uses default benchmark and default files)
+
+EOF
+}
+
 
 # Default input and default files
 default_file="./../conf/defaults.json"
 input_file="./../conf/benchmark.cfg"
+scirpt_file="./pre_test_script.sh"
 
 # Parse command line arguments
-while getopts "d:i:s:" opt; do
+while getopts "d:b:s:" opt; do
   case $opt in
     d) default_file=$OPTARG ;;
-    i) input_file=$OPTARG ;;
+    b) input_file=$OPTARG ;;
     s) script_file=$OPTARG ;;
-    *) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
+    *) Usage; echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
   esac
 done
+
 
 # Call the main program
 python3 main.py "${input_file}" "${default_file}" "${script_file}"
