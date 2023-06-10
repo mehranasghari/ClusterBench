5dae494817627d2d4f2496089dcc8fed90982da3
mehran <mhr.as1317@gmail.com>
2023-06-10 14:26:13 +0330

    Update help for send_load.sh

--- app/send_load.sh
+++ app/send_load.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-# Default values
+# send_load.sh - A script for sending load to a cluster
+
+# Usage:
+#   send_load.sh [OPTIONS]
+
+# Options:
+#   -d, --default-file <file>   : Path to the default file. (Default: ./../conf/defaults.json)
+#   -i, --input-file <file>     : Path to the input file. (Default: ./../conf/benchmark.cfg)
+#   -s, --script-file <file>    : Path to the script file.
+
+# Description:
+#   This script sends load to a cluster based on the provided input and default files.
+
+# Example usage:
+#   send_load.sh -d /path/to/defaults.json -i /path/to/benchmark.cfg -s /path/to/script.sh
+#   send_load.sh -s /path/to/script.sh    (uses default input and default files)
+
+# Default input and default files
 default_file="./../conf/defaults.json"
 input_file="./../conf/benchmark.cfg"
 
