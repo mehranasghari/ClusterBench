40fa5cdd5d96fdee42e064e863a47d866602e4cc
mehran <mhr.as1317@gmail.com>
2023-06-10 13:58:32 +0330

    Remove additional-scripts.sh file

--- Prerequests/pip-installer.sh
+++
@@ -1,5 +0,0 @@
-echo "*-*-*-*-*-*-*-*- Installing Pip -*-*-*-*-*-*-*-*-*-*"
-sudo apt install python3-pip
-sudo pip3 install --upgrade pip 
-sudo pip install pandas
-echo "*-*-*-*-*-*-*-*- DONE -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
