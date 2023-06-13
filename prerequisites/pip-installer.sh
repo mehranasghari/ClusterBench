3f64f41eff553506c9da9a4163036e2c01f3b955
alireza-hdri <alirezaheydari617@yahoo.com>
2023-06-13 12:03:04 +0330

    pip updated

--- prerequisites/pip-installer.sh
+++ prerequisites/pip-installer.sh
@@ -1,4 +1,5 @@
 echo "*-*-*-*-*-*-*-*- Installing Pip -*-*-*-*-*-*-*-*-*-*"
 sudo apt install python3-pip
 sudo pip3 install --upgrade pip 
+pip install influxdb
 echo "*-*-*-*-*-*-*-*- DONE -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
