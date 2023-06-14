4bdd223788e2097e045170e75c38c94d6082be77
alireza-hdri <alirezaheydari617@yahoo.com>
2023-06-14 18:39:59 +0330

    add Restore and Backup directories

--- prerequisites/pip-installer.sh
+++ prerequisites/pip-installer.sh
@@ -1,6 +1,6 @@
 echo "*-*-*-*-*-*-*-*- Installing Pip -*-*-*-*-*-*-*-*-*-*"
-sudo apt install python3-pip
-sudo pip3 install --upgrade pip 
+apt install python3-pip
+pip3 install --upgrade pip 
 pip install influxdb
 pip install pytz
 echo "*-*-*-*-*-*-*-*- DONE -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
