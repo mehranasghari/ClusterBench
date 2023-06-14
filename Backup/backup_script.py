d0e7cf522069f3b5c0d61e3a89e1ed33f5f52c0e
alireza-hdri <alirezaheydari617@yahoo.com>
2023-06-14 17:17:40 +0330

    do some changes

--- Backup/backup_script.py
+++ Backup/backup_script.py
@@ -2,6 +2,8 @@ import datetime
 import os
 import subprocess
 import argparse
+from influxdb import InfluxDBClient
+import os
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-t", "--testname", help="Test Name (Directory in Result/)")
@@ -140,19 +142,43 @@ extract_tar_gz(file_path, extraction_path)
 
 print("------------------ Start Restore  ------------------")
 command2 = "influxd restore -portable /var/lib/influxdb/untarred-files/"
-os.system(f"docker exec -it {container_name} {command2} > /dev/null")
+#os.system(f"docker exec -it {container_name} {command2} > /dev/null")
+completed_process = subprocess.run(["docker", "exec", "-it", container_name, command2], stdout=subprocess.DEVNULL)
+
+if completed_process.returncode == 0:
+    print("\033[92mRestore Done Successfully.\033[0m")  # Print green message
+else:
+    print("\033[91mRestore Failed failed.\033[0m")  # Print red message
+
 print("------------------ END Restore  ------------------")
 
 
 print("------------ Start remove files ------------")
-command3 = "rm -rf /mnt/sdb/influx-test/influxdb-data/untarred-files/*"
-os.system(f"{command3}")
+#command3 = "rm -rf /mnt/sdb/influx-test/influxdb-data/untarred-files/*"
+#os.system(f"{command3}")
+command3 = "rm -rf /mnt/sdb/influx-test/influxdb-data/untarred-files/"
+
+completed_process = subprocess.run(command3, shell=True)
+
+if completed_process.returncode == 0:
+    print("\033[92mFiles removes successfully.\033[0m")  # Print green message
+else:
+    print("\033[91mRemoving files failed.\033[0m")  # Print red message
 print("------------ END remove files --------------------")
 
 
 print("------------ Start moving file ------------")
-command4 = f"mv /root/monster/hayoola-mc/influxdb-data/test-backup/{file_name}  /mnt/sdb/influx-test/influxdb-data/tarred-files/"
-os.system(f"{command4}")
+#command4 = f"mv /root/monster/hayoola-mc/influxdb-data/test-backup/{file_name}  /mnt/sdb/influx-test/influxdb-data/tarred-files/"
+#os.system(f"{command4}")
+command4 = f"mv /root/monster/hayoola-mc/influxdb-data/test-backup/{file_name} /mnt/sdb/influx-test/influxdb-data/tarred-files/"
+
+completed_process = subprocess.run(command4, shell=True)
+
+if completed_process.returncode == 0:
+    print("\033[92mFile moved successfully.\033[0m")  # Print green message
+else:
+    print("\033[91mFailed to move the file.\033[0m")  # Print red message
+
 print("------------ end moving file ------------")
 
 print ("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* END RESTORE *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
--- Restore/function.py
+++ Restore/function.py
@@ -1,31 +1,35 @@
-# Specify the file path and name
-file_path = './result.py'
+import csv
 
-# Open the file
-with open(file_path, 'r') as file:
-    # Read the first line
-    first_line = file.readline().strip()
-    
-    # Evaluate the line as a Python expression
-    data = eval(first_line)
-    
-    # Initialize variables for sum and count
-    total_mean = 0
-    count = 0
-    
-    # Iterate over the dictionaries and calculate the sum and count
-    for item in data:
-        mean = item['mean']
-        total_mean += mean
-        count += 1
-    
-    # Calculate the average
-    if count > 0:
-        average = total_mean / count
-    else:
-        average = 0
-    
-    # Print the result
-    print("Sum of mean values:", total_mean)
-    print("Count:", count)
-    print("Average:", average)
+# Specify the file paths
+input_file_path = './result.txt'
+output_file_path = './file.csv'
+hosts = './hosts.txt'
+
+# Open the input and output files
+with open(input_file_path, 'r') as input_file, open(output_file_path, 'w', newline='') as output_file:
+    # Create a CSV writer
+    writer = csv.writer(output_file)
+
+    # Iterate over each line in the input file
+    for line in input_file:
+        # Strip any leading/trailing whitespace
+        line = line.strip()
+
+        # Evaluate the line as a Python expression
+        data = eval(line)
+
+        # Calculate the sum and count
+        total_mean = sum(item['mean'] for item in data)
+        count = len(data)
+
+        # Calculate the average
+        if count > 0:
+            average = total_mean / count
+        else:
+            average = 0
+
+        # Write the mean value to the CSV file
+        writer.writerow([average])
+
+# Print a message to indicate the CSV file has been created
+print(f"Results saved to {output_file_path}")
