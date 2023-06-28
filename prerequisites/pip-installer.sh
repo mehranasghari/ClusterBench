aab4d43f3a1d6071e0687b776f7f6cde40d37435
alireza-hdri <alirezaheydari617@yahoo.com>
2023-06-28 15:38:59 +0330

    prerequisite dictaion corrected

--- prerequisites/pip-installer.sh
+++
@@ -1,6 +0,0 @@
-echo "*-*-*-*-*-*-*-*- Installing Pip -*-*-*-*-*-*-*-*-*-*"
-apt install python3-pip
-pip3 install --upgrade pip 
-pip install influxdb
-pip install pytz
-echo "*-*-*-*-*-*-*-*- DONE -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
