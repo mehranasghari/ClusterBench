484bd53364ec3ff5888888fcc32028550f1539a6
alireza-hdri <alirezaheydari617@yahoo.com>
2023-07-04 10:14:20 +0330

    convertor function place change under the host iteration

--- query-convertor/convertor2.py
+++ query-convertor/convertor2.py
@@ -138,7 +138,46 @@ for dir_backup in backup_dir_list:
 
             # Iterate over each host and execute code
             for host in hosts:
-                  query = influxdb_queries
+                  # Convert json to influx query
+                  def convert_panel_json_to_influxdb_query(panel_json):
+                      # Load the JSON from file
+                      json_data = json.loads(panel_json)
+
+                      # Extract query information
+                      targets = json_data.get("targets", [])
+
+                      influxdb_queries = []
+
+                      # Process each query target
+                      for target in targets:
+                          measurement = target.get("measurement")
+                          tags = target.get("tags", [])
+
+                          # Construct the measurement and tags portion of the query
+                          measurement_query = f'"{measurement}"'
+
+                          tag_queries = []
+                          for tag in tags:
+                              tag_name = tag.get("key")
+                              tag_value = tag.get("value")
+                              tag_operator = tag.get("operator", "=")
+                              tag_query = f'("{tag_name}" {tag_operator} {tag_value})'
+                              tag_queries.append(tag_query)
+
+                          tags_query = " AND ".join(tag_queries)
+
+                          # Construct the complete InfluxDB query
+                          influxdb_query = f'SELECT mean("value") FROM {measurement_query} WHERE ("host" =~ /^{host}$/)'+" AND time >= {start_time_query}ms and time <= {end_time_query}ms GROUP BY {group_by} fill(null);"
+                          influxdb_queries.append(influxdb_query)
+
+                          # Read panel JSON from file
+                          with open(query_file_path, "r") as panel_file:
+                              panel_json = panel_file.read()
+
+                          influxdb_queries = convert_panel_json_to_influxdb_query(panel_json)
+                          influxdb_queries = str(influxdb_queries)
+                          influxdb_queries = influxdb_queries.strip("[]")
+                          query = influxdb_queries
 
                   # Run the query by variables
                   query = query.format(group_by=group_by,host=host,start_time_query=start_time_query,end_time_query=end_time_query)
