511199d823c95ea8d9c76ba45bac9fe9ed3a3c87
alireza-hdri <alirezaheydari617@yahoo.com>
2023-07-11 11:40:14 +0330

    2.py query generating and reading ability added

--- query-convertor/2.py
+++ query-convertor/2.py
@@ -13,6 +13,7 @@ from datetime import datetime
 # Specify address to config files
 address_file_path = "./../conf/address.json"
 hosts_file_path = "./../Hosts/hosts-test.txt"
+query_file_path = "./query.txt"
 
 # Load the JSON data from the file and define addresses as a variable
 with open(address_file_path, 'r') as file:
@@ -29,12 +30,25 @@ args = argParser.parse_args()
 directorypath = args.directorypath
 backup_dir_list = os.listdir(directorypath)
 
+# Generate query
+query_generator_command = 'python3 convertor.py'
+query_generator_process = subprocess.run(query_generator_command, shell=True)
+query_exit_code = query_generator_process.returncode
+
 # iterate over hosts
 with open(hosts_file_path, 'r') as file:
     hosts = file.readlines()  # Read the hosts from the file
     hosts = [host.strip() for host in hosts]  # Remove any whitespace characters from the end of each line
-    
     for backup_dir in backup_dir_list:     
+    
+            print(f"*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* START OF Restore FOR\033[92m {backup_dir} \033[0m*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+            if query_exit_code == 0:
+                print(f"\033[92mQuery Generated successfully.\033[0m")
+                print()
+            else:
+                print(f"\033[91mQuery Generating failed.\033[0m")
+                print()
+                break            
 
             # Drop DB
             drop_command = f"docker exec -it {Secondary_influxdb_container_name} influx -execute 'drop database {Secondary_influxdb_DB_name}'"
@@ -99,5 +113,12 @@ with open(hosts_file_path, 'r') as file:
                     datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S").astimezone(datetime.now().astimezone().tzinfo).timetuple()) * 1000
             
             for host in hosts:
-                continue
+                with open(query_file_path) as file:
+                    query= file.read()
+                    print(query)
+
+
+    print(f"*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* END OF Restore FOR\033[92m {backup_dir} \033[0m*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+
+
 
