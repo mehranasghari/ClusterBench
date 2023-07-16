2cf5ada2d63d9b0f494af72eb17a8eb213202553
alireza-hdri <alirezaheydari617@yahoo.com>
2023-07-16 14:56:37 +0330

    some changes

--- Api/api.py
+++ Api/api.py
@@ -15,8 +15,23 @@ Secondary_influxdb_container_name = json_data['Secondary_influxdb_container_name
 Secondary_influxdb_DB_name = json_data['Secondary_influxdb_DB_name']
 Secondary_influxdb_address_in_host = json_data['Secondary_influxdb_address_in_host']
 
-def query_parser(grafana_token_file_path):
-    with open (grafana_token_file_path, 'w') as file:
-        token = file.read
+def query_parser(grafana_token_file_path,query_file_path):
     
-    curl -H "Authorization: Bearer eyJrIjoiUkpqZUNDRUxVSkxXWXBJaTJoMDB6djdSR1dlczI0VDMiLCJuIjoidGVzdCBmb3IgY29kZSIsImlkIjoxfQ==" http://157.119.190.139:3000/api/dashboards/home
+    # Read api key
+    with open (grafana_token_file_path, 'r') as file:
+        token = file.read()
+    
+    # Read queries and loop them
+    with open (query_file_path, 'r') as file :
+        lines = file.readlines()
+        for line in lines:
+            curl_command = f'curl -H "Authorization: Bearer {token}" > /dev/null'
+            curl_process = subprocess.run(curl_command,shell=True)
+            exit_code = curl_process.returncode
+            if exit_code == 0:
+                print("done")
+            else:
+                print("undone")
+            
+query_parser(grafana_token_file_path,query_file_path)
+    #curl -H "Authorization: Bearer eyJrIjoiUkpqZUNDRUxVSkxXWXBJaTJoMDB6djdSR1dlczI0VDMiLCJuIjoidGVzdCBmb3IgY29kZSIsImlkIjoxfQ==" http://157.119.190.139:3000/api/dashboards/home
