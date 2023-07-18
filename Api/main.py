3c786381659fe4a2f7267d4ae19992485e77530f
alireza-hdri <alirezaheydari617@yahoo.com>
2023-07-18 09:31:45 +0330

    url problem resovled

--- Api/main.py
+++ Api/main.py
@@ -22,7 +22,7 @@ with open(measurment_file_path, 'r') as file :
         with open(hosts_file_path,'r') as file:
              hosts = file.readlines
              for host in hosts:
-                curl_command = f'curl -G 'http://{host}:{port}/query?pretty=true' --data-urlencode "db={DB_name}" --data-urlencode "q=SELECT mean("value") FROM "{measurment}" WHERE ("host" =~ /^{host}$/) AND time >= now() - 30s AND time <= now() GROUP BY time(10s) fill(none)"'
+                curl_command = f'curl -G \'http://{host}:{port}/query?pretty=true\' --data-urlencode "db={DB_name}" --data-urlencode "q=SELECT mean("value") FROM "{measurment}" WHERE ("host" =~ /^{host}$/) AND time >= now() - 30s AND time <= now() GROUP BY time(10s) fill(none)"'
                 curl_process = subprocess.run(curl_command, shell=True)
                 curl_exit_code = curl_process.returncode()
                 if curl_exit_code == 0:
