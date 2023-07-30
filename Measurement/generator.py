3c1972929bb24bdb4c1a19f33ffc00998d55470e
alireza-hdri <alirezaheydari617@yahoo.com>
2023-07-30 09:47:49 +0330

    generator and generated metric updated

--- Measurement/generator.py
+++ Measurement/generator.py
@@ -12,15 +12,28 @@ def measurement_generator(input_value):
                     with open(metrics_file_path, "a") as metrics_file:
                         metrics_file.write(f"netdata.disk_ops.{disk.strip()}.reads\n")
                         metrics_file.write(f"netdata.disk_ops.{disk.strip()}.write\n")
+            print("\033c", end="")
         case "2":
             print("You chose option 2.")
+            # Implement functionality for option 2
         case "3":
-            print("You chose option 3.")
+            print("You chose option 2.")
+
+        case "exit": 
+            print("Exiting the program.")
         case _:
             print("Invalid option.")
 
-# Example usage:
-print(''' HINT
-1) OPS measurements(read and write) ''')
-user_input = input("\n Enter your choice: ")
-measurement_generator(user_input)
+# Example usage with loop:
+while True:
+    print('''
+      Which want to generate ? 
+      
+      1) OPS (read and write)
+      Enter "exit" to exit.''')
+    user_input = input("\n Enter your choice: ").lower()  # Convert input to lowercase for case-insensitivity
+
+    if user_input == "exit":
+        break  # Exit the loop if user enters "exit"
+    
+    measurement_generator(user_input)
