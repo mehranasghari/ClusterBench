4878ba3bfd149eead0e67e196b53a2aebdda1e4f
alireza-hdri <alirezaheydari617@yahoo.com>
2023-07-30 15:58:29 +0330

    input content variabelised

--- app/generate_xml_tester.py
+++ app/generate_xml_tester.py
@@ -3,6 +3,15 @@ import unittest
 import os
 from generate_xml import convert_input_to_xml
 
+iw = 10
+ics = 1
+ice = 5
+pw = 1
+pcs = 1
+pce = 10
+s = 100
+pos = 1
+poe = 50
 class TestConvertInputToXml(unittest.TestCase):
     def setUp(self):
         # Create temporary input and defaults files for testing
@@ -18,7 +27,7 @@ class TestConvertInputToXml(unittest.TestCase):
 
     def test_convert_input_to_xml(self):
         # Create a sample input and defaults JSON for testing
-        input_content = 'init,workers=10,containers=1-5\nprepare,workers=5,containers=1-10,objects=1-20,sizes=100\n'
+        input_content = f'init,workers={iw},containers={ics}-{ice}\nprepare,workers={pw},containers={pcs}-{pce},objects={pos}-{poe},sizes={s}\n'
         defaults_content = '{"cluster_address": "localhost", "workers": 1, "start_containers": 1, "end_containers": 10, "start_objects": 1, "end_objects": 5, "sizes": 200, "put": 50, "get": 30, "delete": 20, "delay": 2}'
         
         with open(self.input_file, 'w') as f_input:
@@ -34,7 +43,7 @@ class TestConvertInputToXml(unittest.TestCase):
         with open(self.output_file, 'r') as f_output:
             output_content = f_output.read()
 
-        expected_output = '<?xml version="1.0" encoding="UTF-8" ?>\n<workload name="swift-sample" description="sample benchmark for swift">\n  <storage type="swift" />\n  <auth type="swauth" config="username=test:tester;password=testing;auth_url=http://localhost:8080/auth/v1.0" />\n  <workflow>\n\n    <workstage name="init">\n      <work type="init" workers="10" config="containers=r(1,5)" />\n    </workstage>\n\n    <workstage name="prepare">\n      <work type="prepare" workers="5" config="containers=r(1,10);objects=r(1,20);sizes=c(100)KB" />\n    </workstage>\n\n  </workflow>\n</workload>\n'
+        expected_output = f'<?xml version="1.0" encoding="UTF-8" ?>\n<workload name="swift-sample" description="sample benchmark for swift">\n  <storage type="swift" />\n  <auth type="swauth" config="username=test:tester;password=testing;auth_url=http://localhost:8080/auth/v1.0" />\n  <workflow>\n\n    <workstage name="init">\n      <work type="init" workers="{iw}" config="containers=r({ics},{ice})" />\n    </workstage>\n\n    <workstage name="prepare">\n      <work type="prepare" workers="{pw}" config="containers=r({pcs},{pce});objects=r({pos},{poe});sizes=c({s})KB" />\n    </workstage>\n\n  </workflow>\n</workload>\n'
         self.assertEqual(output_content, expected_output)
 
 if __name__ == '__main__':
