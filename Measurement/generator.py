f53b2919411cd89ff238ebd9abeda3ee173b61cf
alireza-hdri <alirezaheydari617@yahoo.com>
2023-08-01 07:25:14 +0330

    generator updated

--- Measurement/generator.py
+++ Measurement/generator.py
@@ -18,9 +18,8 @@ db_port = json_data["Primary_influxdb_container_port"]
 db_host = json_data["Priamry_influxdb_host_in_container"]
 
 # execute to container and save the input into the measurement.txt file
-#exec_command = f"docker exec -it {influxdb_container_name} influx -host {db_host} -port {db_port} -database '{db_name}' -execute 'SHOW MEASUREMENTS' > {exported_measurements_file_path} "
-#os.system(exec_command)
-# Assuming the execution of the command was successful, no need to check the exit code here.
+exec_command = f"docker exec -it {influxdb_container_name} influx -host {db_host} -port {db_port} -database '{db_name}' -execute 'SHOW MEASUREMENTS' > {exported_measurements_file_path} "
+os.system(exec_command)
 
 # Metric file generator
 with open(exported_measurements_file_path, "r") as measurements_file:
