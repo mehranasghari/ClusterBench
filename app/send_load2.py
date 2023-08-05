313a2724b54ce847b346a3a06f4387d249dce75e
alireza-hdri <alirezaheydari617@yahoo.com>
2023-08-05 08:47:52 +0330

    send_load2.py modified

--- app/send_load2.py
+++ app/send_load2.py
@@ -2,27 +2,29 @@ import argparse
 import os 
 import subprocess 
 
-def main(benchmark_file, default_file, script_file):
-    # Your main logic here
-    print(f"Benchmark File: {benchmark_file}")
-    print(f"Default File: {default_file}")
-    print(f"Script File: {script_file}")
+def main():
+    parser = argparse.ArgumentParser()
+    
+    # Define the arguments with default values
+    parser.add_argument('-d', '--arg_b', default="./../conf/defaults.json", help="Path to the default file.")
+    parser.add_argument('-b', '--arg_d', default="./../conf/benchmark.cfg", help="Path to the benchmark file.")
+    parser.add_argument('-s', '--arg_s', default="./pre_test_script.sh", help="Path to the script file.")
 
-if __name__ == "__main__":
-    # Create the argument parser
-    parser = argparse.ArgumentParser(description="A script for sending load to a cluster")
-
-    # Add the command-line options
-    parser.add_argument("-d", "--default-file", dest="default_file", default="./../conf/defaults.json", help="Path to the default file.")
-    parser.add_argument("-b", "--benchmark-file", dest="benchmark_file", default="./../conf/benchmark.cfg", help="Path to the benchmark file.")
-    parser.add_argument("-s", "--script-file", dest="script_file", default="./pre_test_script.sh", help="Path to the script file.")
-
-    # Parse the command-line arguments
     args = parser.parse_args()
 
-    # Call the main program with the provided arguments
-    main(args.benchmark_file, args.default_file, args.script_file)
+    # Access the argument values
+    benchmark_file = args.arg_b
+    default_file = args.arg_d
+    script_file = args.arg_s
 
-# call main.py
-call_command = f" python3 main.py {args.benchmark_file} {args.default_file} {args.script_file}"
-call_process = subprocess.run(call_command, shell=True)
+    # Use the argument values in your application logic
+    print(f'Argument B: {benchmark_file}')
+    print(f'Argument D: {default_file}')
+    print(f'Argument S: {script_file}')
+    
+    # Run main.py
+    call_command = f" python3 main.py {benchmark_file} {default_file} {script_file}"
+    call_process = subprocess.run(call_command, shell=True)
+
+if __name__ == '__main__':
+    main()
