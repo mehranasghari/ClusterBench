4e683d1fd5b375db124cf6be404edec5027b529f
alireza-hdri <alirezaheydari617@yahoo.com>
2023-08-05 09:03:38 +0330

    send_load2.py modified

--- app/send_load2.py
+++ app/send_load2.py
@@ -1,30 +1,54 @@
-import argparse
-import os 
-import subprocess 
+#!/usr/bin/env python3
 
-def main():
-    parser = argparse.ArgumentParser()
+import sys
+import getopt
+import os
+import subprocess
+
+def usage():
+    print("""
+    send_load.py [OPTIONS]
+
+Options:
+  -d, --default-file <file>   : Path to the default file. (Default: ./../conf/defaults.json)
+  -b, --benchmark-file <file> : Path to the benchmark file. (Default: ./../conf/benchmark.cfg)
+  -s, --script-file <file>    : Path to the script file. (Default: ./pre_test_script.sh)
+
+Description:
+  This script sends load to a cluster based on the provided benchmark and default files.
+  Script-file is executed before every test.
+
+Example usage:
+  send_load.py -d /path/to/defaults.json -b /path/to/benchmark.cfg -s /path/to/script.sh
+  send_load.py -s /path/to/script.sh    (uses default benchmark and default files)
+""")
+
+
+def main(argv):
+    # Default input and default files
+    default_file = "./../conf/defaults.json"
+    benchmark_file = "./../conf/benchmark.cfg"
+    script_file = "./pre_test_script.sh"
+
+    # Parse command line arguments
+    try:
+        opts, args = getopt.getopt(argv, "hd:b:s:", ["default-file=", "benchmark-file=", "script-file="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(1)
+
+    for opt, arg in opts:
+        if opt in ("-d", "--default-file"):
+            default_file = arg
+        elif opt in ("-b", "--benchmark-file"):
+            benchmark_file = arg
+        elif opt in ("-s", "--script-file"):
+            script_file = arg
+
+    # Call the main program 
+    print(f"Calling main program with benchmark_file: {benchmark_file}, default_file: {default_file}, script_file: {script_file}")
+    run = f"python3 main.py {benchmark_file} {default_file} {script_file}"
+    run_process = subprocess.run(run,shell=True)
     
-    # Define the arguments with default values
-    parser.add_argument('-d', '--arg_d', default="./../conf/defaults.json", help="Path to the default file.")
-    parser.add_argument('-b', '--arg_b', default="./../conf/benchmark.cfg", help="Path to the benchmark file.")
-    parser.add_argument('-s', '--arg_s', default="./pre_test_script.sh", help="Path to the script file.")
-
-    args = parser.parse_args()
-
-    # Access the argument values
-    benchmark_file = args.arg_b
-    default_file = args.arg_d
-    script_file = args.arg_s
-
-    # Use the argument values in your application logic
-    print(f'Argument B: {benchmark_file}')
-    print(f'Argument D: {default_file}')
-    print(f'Argument S: {script_file}')
-    
-    # Run main.py
-    call_command = f" python3 main.py {benchmark_file} {default_file} {script_file}"
-    call_process = subprocess.run(call_command, shell=True)
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    main(sys.argv[1:])
