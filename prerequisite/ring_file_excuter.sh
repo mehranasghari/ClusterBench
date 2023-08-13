4429e42e6272e04cf25a62099a1814476b451f08
Alireza Heydari <109125333+alireza-hdri@users.noreply.github.com>
2023-08-13 14:20:20 +0330

    ssh host updated (#114)
    * script.sh updated
    
    * test for bug resolvation
    
    * test for bug resolvation
    
    * prerequiste updated with ability to generate ring excuter script
    
    * script file updated
    
    * delete some extra files
    
    * code commented
    
    * output per if changed
    
    * attention message place changed
    
    * attention message place changed
    
    * ring file excuation added to file
    
    * script name execation changed
    
    * delete and edit in swift commands
    
    * script extention changed from py to sh
    
    * output per operation managed
    
    * output changed due to problem
    
    * if updated with elseses
    
    * extra forwarding to /dev/null deleted
    
    * ring-file-excuter updated
    
    * ssh host updated
    
    * ssh command updated

--- prerequisite/ring_file_excuter.sh
+++
@@ -1,3 +0,0 @@
-docker exec test bash -c "swift-ring-builder /rings/account.builder" > ./account.txt
-docker exec test bash -c "swift-ring-builder /rings/object.builder" > ./object.txt
-docker exec test bash -c "swift-ring-builder /rings/container.builder" > ./container.txt
