763a3fdb97e36282c4d87bdf9515cd00b8d379ed
alireza-hdri <alirezaheydari617@yahoo.com>
2023-08-20 12:21:16 +0330

    change on defult.json

--- Codes/get_conf.py
+++ Codes/get_conf.py
@@ -14,6 +14,7 @@ def get_conf(host_file_path):
             monster_host_ip = words[1].strip()
             monster_container_name = words[2].strip()
 
+            
 
 if __name__ == "__main__":
     # Parse command-line arguments
