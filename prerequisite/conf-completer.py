03c7a54c6d3b2a85c0f15cd9c8862452a3154825
alireza-hdri <alirezaheydari617@yahoo.com>
2023-08-28 11:02:17 +0330

    change on command

--- prerequisite/conf-completer.py
+++ prerequisite/conf-completer.py
@@ -4,9 +4,9 @@ import subprocess
 import json
 
 # Search and find main influxdb mount points
-influxdb_container_name = input(f"Please enter your InfluxDB container name (Default :storage ): ")
+influxdb_container_name = input(f"Please enter your InfluxDB container name (Default :influxdb ): ")
 if influxdb_container_name == "":
     influxdb_container_name = "influxdb"
-main_mount_command = f"docker inspect -f '{{range .Mounts}}{{if eq .Mode \"rw\"}}{{.Source}} {{.Destination}}{{end}}{{end}}' {influxdb_container_name}"
-main_mount_process = subprocess.run(main_mount_command, shell=True, capture_output=True, text=True)
-print("\n",main_mount_process.stdout)
+#main_mount_command = f"docker inspect -f '{{range .Mounts}}{{if eq .Mode \"rw\"}}{{.Source}} {{.Destination}}{{end}}{{end}}' {influxdb_container_name}"
+#main_mount_process = subprocess.run(main_mount_command, shell=True)
+os.system(f"docker inspect -f '{{range .Mounts}}{{if eq .Mode \"rw\"}}{{.Source}} {{.Destination}}{{end}}{{end}}' {influxdb_container_name}")
