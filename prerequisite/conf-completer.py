812547de0af0d93031a8221bdf9c08e46aed7707
alireza-hdri <alirezaheydari617@yahoo.com>
2023-08-28 10:50:07 +0330

    change on output

--- prerequisite/conf-completer.py
+++ prerequisite/conf-completer.py
@@ -8,5 +8,5 @@ influxdb_container_name = input(f"Please enter your InfluxDB container name (Def
 if influxdb_container_name == "":
     influxdb_container_name = "influxdb"
 main_mount_command = f"docker inspect -f '{{range .Mounts}}{{if eq .Mode \"rw\"}}{{.Source}} {{.Destination}}{{end}}{{end}}' {influxdb_container_name}"
-main_mount_process = subprocess.run(main_mount_command, shell=True)
-print("\n",main_mount_command)
+main_mount_process = subprocess.run(main_mount_command, shell=True, capture_output=True, text=True)
+print("\n",main_mount_process.stdout)
