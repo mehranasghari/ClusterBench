3b794226d56c3a314bf4a3cba469c67a6ee67f4c
alireza-hdri <alirezaheydari617@yahoo.com>
2023-08-28 12:06:51 +0330

    sample json file added and fucntion updated to add value to json file

--- prerequisite/conf-completer.py
+++ prerequisite/conf-completer.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import json
 
-influxdb_container_name = input("Please enter your  main InfluxDB container name (Default: influxdb): ")
+influxdb_container_name = input("Please enter your main InfluxDB container name (Default: influxdb): ")
 if influxdb_container_name == "":
     influxdb_container_name = "influxdb"
 
@@ -10,7 +10,20 @@ if influxdb_container_name == "":
 main_mount_point_command = f'docker inspect -f "{{{{range .Mounts}}}}{{{{if eq .Mode \\"rw\\"}}}}{{{{.Source}}}} {{{{.Destination}}}}{{{{end}}}}{{{{end}}}}" {influxdb_container_name}'
 main_mount_point_process = subprocess.run(main_mount_point_command, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
 
-# Extracting and printing the output
+# Extracting and splitting the output
 main_mount_point = main_mount_point_process.stdout.strip()
-first_address , second_address = main_mount_point.strip().split(" ")
-print("first_address : ", first_address)
+first_address, second_address = main_mount_point.strip().split(" ")
+
+# Load existing JSON
+with open("data.json", "r") as json_file:
+    existing_json = json.load(json_file)
+
+# Add addresses to the existing JSON
+existing_json["first_address"] = first_address
+existing_json["second_address"] = second_address
+
+# Save back the modified JSON
+with open("data.json", "w") as json_file:
+    json.dump(existing_json, json_file, indent=4)
+
+print("Addresses added to JSON.")
