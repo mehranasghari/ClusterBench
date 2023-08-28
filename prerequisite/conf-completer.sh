146dc8d1d932a72cfca8bb1cab402014a97a53bc
alireza-hdri <alirezaheydari617@yahoo.com>
2023-08-28 11:33:41 +0330

    test.json added for testing some function on json

--- prerequisite/conf-completer.sh
+++ prerequisite/conf-completer.sh
@@ -9,8 +9,16 @@ fi
 mount_output=$(docker inspect -f '{{range .Mounts}}{{if eq .Mode "rw"}}{{.Source}} {{.Destination}}{{end}}{{end}}' "$influxdb_container_name")
 
 # Use read to split the output into two variables
-read -r source_path destination_path <<< "$mount_output"
+read -r Main_influxdb_address_in_host Main_influxdb_in_container_address <<< "$mount_output"
 
-# Print the captured output
-echo "Source Path: $source_path"
-echo "Destination Path: $destination_path"
+# Read the existing JSON file
+existing_json=$(cat ./test.json)
+
+# Create a new JSON object with the new values
+new_json=$(jq --arg sp "$source_path" --arg dp "$destination_path" \
+             '. + {"new_key": {"Main_influxdb_address_in_host": $sp, "Main_influxdb_in_container_address": $dp}}' <<< "$existing_json")
+
+# Write the modified JSON back to the file
+echo "$new_json" > existing.json
+
+echo "JSON file 'existing.json' updated with new values."
