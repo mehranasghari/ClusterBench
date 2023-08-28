afbc36d7595eff7f7f42fdbd471a992bca40f12f
alireza-hdri <alirezaheydari617@yahoo.com>
2023-08-28 11:20:02 +0330

    variable defined

--- prerequisite/conf-completer.sh
+++ prerequisite/conf-completer.sh
@@ -5,4 +5,12 @@ if [ -z "$influxdb_container_name" ]; then
     influxdb_container_name="influxdb"
 fi
 
-docker inspect -f '{{range .Mounts}}{{if eq .Mode "rw"}}{{.Source}} {{.Destination}}{{end}}{{end}}' $influxdb_container_name
+# Capture the output of the docker inspect command into a variable
+mount_output=$(docker inspect -f '{{range .Mounts}}{{if eq .Mode "rw"}}{{.Source}} {{.Destination}}{{end}}{{end}}' "$influxdb_container_name")
+
+# Use read to split the output into two variables
+read -r source_path destination_path <<< "$mount_output"
+
+# Print the captured output
+echo "Source Path: $source_path"
+echo "Destination Path: $destination_path"
