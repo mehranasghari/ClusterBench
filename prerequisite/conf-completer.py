16ea9e862ce6289f8d4377fbfb833ff201ad895d
alireza-hdri <alirezaheydari617@yahoo.com>
2023-08-28 10:46:37 +0330

    container name variable added to docker commnad

--- prerequisite/conf-completer.py
+++ prerequisite/conf-completer.py
@@ -7,6 +7,6 @@ import json
 influxdb_container_name = input(f"Please enter your InfluxDB container name (Default :storage ): ")
 if influxdb_container_name == "":
     influxdb_container_name = "influxdb"
-main_mount_command = f"docker inspect -f \'{{range .Mounts}}{{if eq .Mode \"rw\"}}{{.Source}} {{.Destination}}{{end}}{{end}}\' "
+main_mount_command = f"docker inspect -f '{{range .Mounts}}{{if eq .Mode \"rw\"}}{{.Source}} {{.Destination}}{{end}}{{end}}' {influxdb_container_name}"
 main_mount_process = subprocess.run(main_mount_command, shell=True)
 print("\n",main_mount_command)
