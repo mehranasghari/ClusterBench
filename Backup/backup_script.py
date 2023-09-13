9e29e5f56e04ed9254a3c9efbf9ebf6504b5a702
alireza-hdri <alirezaheydari617@yahoo.com>
2023-09-13 07:47:27 +0330

    bar process added to backup process file

--- Backup/backup_script.py
+++ Backup/backup_script.py
@@ -6,6 +6,7 @@ import calendar
 import sys
 import json
 import time
+from alive_progress import alive_bar
 
 # Specify address to address.json file
 influxdb_conf_file_path = "./../conf/Software/InfluxdbConfig.json"
@@ -30,6 +31,8 @@ testDirectory = args.testname
 global testDirectory2
 testDirectory2 = args.testname
 input_file = "./../result/"+testDirectory+"/time"
+ring_dir_in_result_path = "./../result/"+testDirectory+"/Ring_cluster"
+conf_dir_in_result_path = "./../result/"+testDirectory+"/Config_cluster"
 
 #time defenition
 gmt_offset_seconds = 3 * 3600 + 30 * 60
@@ -39,96 +42,115 @@ gmt_offset_seconds = 3 * 3600 + 30 * 60
 
 
 def process_input_file(file_path_input):
+    with alive_bar(16,title=f'Processing Test : \033[92m{testDirectory}\033[0m') as bar:
+        # check if config and ring dir exist
+        if os.path.exists(ring_dir_in_result_path):
+            bar()
+        if os.path.exists(conf_dir_in_result_path):
+            bar()
+        with open(file_path_input, "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                
+                # Split given time
+                start_datetime_str, end_datetime_str = line.strip().split(",")
+                bar()
 
-    with open(file_path_input, "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            
-            # Split given time
-            start_datetime_str, end_datetime_str = line.strip().split(",")
-            
-            # Convert start and end datetime strings to datetime objects
-            start_datetime = datetime.datetime.strptime(start_datetime_str, "%Y-%m-%d %H:%M:%S")
-            end_datetime = datetime.datetime.strptime(end_datetime_str, "%Y-%m-%d %H:%M:%S")
+                # Convert start and end datetime strings to datetime objects
+                start_datetime = datetime.datetime.strptime(start_datetime_str, "%Y-%m-%d %H:%M:%S")
+                end_datetime = datetime.datetime.strptime(end_datetime_str, "%Y-%m-%d %H:%M:%S")
+                bar()
 
-            # Add the GMT+03:30 offset to both datetime objects
-            start_datetime_utc = start_datetime - datetime.timedelta(seconds=gmt_offset_seconds)
-            end_datetime_utc = end_datetime - datetime.timedelta(seconds=gmt_offset_seconds)
+                # Add the GMT+03:30 offset to both datetime objects
+                start_datetime_utc = start_datetime - datetime.timedelta(seconds=gmt_offset_seconds)
+                end_datetime_utc = end_datetime - datetime.timedelta(seconds=gmt_offset_seconds)
+                bar()
 
-            dir_start_datetime_utc = start_datetime - datetime.timedelta(seconds=gmt_offset_seconds)
-            dir_end_datetime_utc = end_datetime - datetime.timedelta(seconds=gmt_offset_seconds)
+                dir_start_datetime_utc = start_datetime - datetime.timedelta(seconds=gmt_offset_seconds)
+                dir_end_datetime_utc = end_datetime - datetime.timedelta(seconds=gmt_offset_seconds)
+                bar()
 
-            # Add the specified number of seconds to both datetime objects
-            start_datetime_utc -= datetime.timedelta(seconds=Time_reduce_from_first_of_test)
-            end_datetime_utc += datetime.timedelta(seconds=Time_add_to_end_of_test)
+                # Add the specified number of seconds to both datetime objects
+                start_datetime_utc -= datetime.timedelta(seconds=Time_reduce_from_first_of_test)
+                end_datetime_utc += datetime.timedelta(seconds=Time_add_to_end_of_test)
+                bar()
 
-            # Convert the UTC datetime objects back to strings
-            start_datetime_utc_str = start_datetime_utc.strftime("%Y-%m-%d %H:%M:%S")
-            end_datetime_utc_str = end_datetime_utc.strftime("%Y-%m-%d %H:%M:%S")
+                # Convert the UTC datetime objects back to strings
+                start_datetime_utc_str = start_datetime_utc.strftime("%Y-%m-%d %H:%M:%S")
+                end_datetime_utc_str = end_datetime_utc.strftime("%Y-%m-%d %H:%M:%S")
+                bar()
 
-            # creating backup time format
-            backup_start_date , backup_start_time = start_datetime_utc_str.split(" ")
-            start_time_backup = backup_start_date+"T"+backup_start_time+"Z"
-            backup_end_date , backup_end_time = end_datetime_utc_str.split(" ")
-            end_time_backup = backup_end_date+"T"+backup_end_time+"Z"
+                # creating backup time format
+                backup_start_date , backup_start_time = start_datetime_utc_str.split(" ")
+                start_time_backup = backup_start_date+"T"+backup_start_time+"Z"
+                backup_end_date , backup_end_time = end_datetime_utc_str.split(" ")
+                end_time_backup = backup_end_date+"T"+backup_end_time+"Z"
+                bar()
 
-            # dir name creation
-            dir_start_date , dir_start_time = start_datetime_str.split(" ")
-            dir_start_date = dir_start_date[2:].replace("-","")
-            dir_start_time = dir_start_time.replace(":","")
-            dir_end_date , dir_end_time = end_datetime_str.split(" ")
-            dir_end_date = dir_end_date[2:].replace("-","")
-            dir_end_time = dir_end_time.replace(":","")
-            backup_dir_name = dir_start_date+"T"+dir_start_time+"_"+dir_end_date+"T"+dir_end_time
-            
-            # Create backup_path2
-            backup_path2 = Primary_influxdb_in_container_address +"/"+ backup_dir_name
-            backup_path = f"{Primary_influxdb_in_container_address}/{backup_dir_name}" 
-            os.makedirs(backup_path, exist_ok=True)
+                # dir name creation
+                dir_start_date , dir_start_time = start_datetime_str.split(" ")
+                dir_start_date = dir_start_date[2:].replace("-","")
+                dir_start_time = dir_start_time.replace(":","")
+                dir_end_date , dir_end_time = end_datetime_str.split(" ")
+                dir_end_date = dir_end_date[2:].replace("-","")
+                dir_end_time = dir_end_time.replace(":","")
+                backup_dir_name = dir_start_date+"T"+dir_start_time+"_"+dir_end_date+"T"+dir_end_time
+                bar()
 
-            # Perform backup using influxd backup command
-            backup_command = f"docker exec -it {Primary_influxdb_container_name} influxd backup -portable -db {Main_influxdb_DB_name} -start {start_time_backup} -end {end_time_backup} {backup_path2}/backup > /dev/null "
-            backup_process = subprocess.run(backup_command, shell=True)
-            exit_code = backup_process.returncode
-            if exit_code == 0:
-                print("\033[92mBackup successful.\033[0m")
-            else:
-                print("\033[91mBackup failed.\033[0m")
-                sys.exit(1)
-            print()
+                # Create backup_path2
+                backup_path2 = Primary_influxdb_in_container_address +"/"+ backup_dir_name
+                backup_path = f"{Primary_influxdb_in_container_address}/{backup_dir_name}" 
+                os.makedirs(backup_path, exist_ok=True)
+                bar()
 
-            # Tar backup files and delete extra files
-            tar_command = f"tar -cf {Primary_influxdb_address_in_host}/{backup_dir_name}/backup.tar.gz -C {Primary_influxdb_address_in_host}/{backup_dir_name}/backup . > /dev/null 2>&1"
-            tar_process = subprocess.run(tar_command, shell=True)
-            exit_code = tar_process.returncode
-            if exit_code == 0:
-                print("\033[92mTar successful.\033[0m")
+                # Perform backup using influxd backup command
+                backup_command = f"docker exec -it {Primary_influxdb_container_name} influxd backup -portable -db {Main_influxdb_DB_name} -start {start_time_backup} -end {end_time_backup} {backup_path2}/backup > /dev/null "
+                backup_process = subprocess.run(backup_command, shell=True)
+                exit_code = backup_process.returncode
+                if exit_code == 0:
+                    #print("\033[92mBackup successful.\033[0m")
+                    bar()
+                else:
+                    print("\033[91mBackup failed.\033[0m")
+                    sys.exit(1)
                 print()
-            else:
-                print("\033[91mTar failed.\033[0m")
-                sys.exit(1)
-                print()
 
-            # Delete backup directory files
-            del_command = f"rm -rf {Primary_influxdb_address_in_host}/{backup_dir_name}/backup/*"
-            del_process = subprocess.run(del_command , shell=True)
+                # Tar backup files and delete extra files
+                tar_command = f"tar -cf {Primary_influxdb_address_in_host}/{backup_dir_name}/backup.tar.gz -C {Primary_influxdb_address_in_host}/{backup_dir_name}/backup . > /dev/null 2>&1"
+                tar_process = subprocess.run(tar_command, shell=True)
+                exit_code = tar_process.returncode
+                if exit_code == 0:
+                    #print("\033[92mTar successful.\033[0m")
+                    #print()
+                    bar()
+                else:
+                    print("\033[91mTar failed.\033[0m")
+                    sys.exit(1)
+                    print()
 
-            # Make info directory and move all into influxdb2 mount points
-            os.makedirs(f"{Primary_influxdb_address_in_host}/{backup_dir_name}/info", exist_ok=True)
-            cp_command = f"cp -r ./../result/{testDirectory}/* {Primary_influxdb_address_in_host}/{backup_dir_name}/info/"
-            cp_process = subprocess.run(cp_command, shell=True)
+                # Delete backup directory files
+                del_command = f"rm -rf {Primary_influxdb_address_in_host}/{backup_dir_name}/backup/*"
+                del_process = subprocess.run(del_command , shell=True)
+                bar()
 
-	        #MV BACKUP.TAR.GZ TO influxdb2 and delete original file
-            os.makedirs(Secondary_influxdb_address_in_host, exist_ok=True)
-            mv_command = f"mv -f {Primary_influxdb_address_in_host}/{backup_dir_name}  {Secondary_influxdb_address_in_host}/" # specified to move to influxdb 
-            mv_process = subprocess.run(mv_command, shell=True)
-            exit_code = mv_process.returncode
-            if exit_code == 0:
-                print(f"\033[92mFiles moved to {Secondary_influxdb_container_name} location successfully.\033[0m")
-            else:
-                print("\033[91mMoving files failed.\033[0m")
-                sys.exit(1)
-                print()
+                # Make info directory and move all into influxdb2 mount points
+                os.makedirs(f"{Primary_influxdb_address_in_host}/{backup_dir_name}/info", exist_ok=True)
+                cp_command = f"cp -r ./../result/{testDirectory}/* {Primary_influxdb_address_in_host}/{backup_dir_name}/info/"
+                cp_process = subprocess.run(cp_command, shell=True)
+                bar()
+
+                #MV BACKUP.TAR.GZ TO influxdb2 and delete original file
+                os.makedirs(Secondary_influxdb_address_in_host, exist_ok=True)
+                mv_command = f"mv -f {Primary_influxdb_address_in_host}/{backup_dir_name}  {Secondary_influxdb_address_in_host}/" # specified to move to influxdb 
+                mv_process = subprocess.run(mv_command, shell=True)
+                exit_code = mv_process.returncode
+                if exit_code == 0:
+                    #print(f"\033[92mFiles moved to {Secondary_influxdb_container_name} location successfully.\033[0m")
+                    bar()
+                else:
+                    print("\033[91mMoving files failed.\033[0m")
+                    sys.exit(1)
+                    print()
 
 process_input_file(input_file)
 print(f"*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* END OF BACKUP FOR\033[92m {testDirectory} \033[0m*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
