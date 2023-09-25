3535198ace89072c41a89c15b49196c40ad35f86
alireza-hdri <alirezaheydari617@yahoo.com>
2023-09-25 10:22:08 +0330

    change on main2.py

--- app/main2.py
+++ app/main2.py
@@ -52,7 +52,7 @@ def process_on_workloads(all_xml_path):
                 continue  # Continue with the next workload if pre-test fails
             
             # Start workload
-            workload_file_path = os.path.join(all_xml_path, workload_dir, "workload.xml")
+            workload_file_path = os.path.join(all_xml_path, workload_dir)
             print(workload_file_path)
             result = subprocess.run(["bash", cosbench_command, submit, workload_file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
             if result.returncode == 1:
