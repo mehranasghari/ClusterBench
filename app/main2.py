35cdb6d503a178c291bb7d2605e000f290ac521e
alireza-hdri <alirezaheydari617@yahoo.com>
2023-09-25 14:38:01 +0330

    change on main2.py

--- app/main2.py
+++ app/main2.py
@@ -70,6 +70,8 @@ def process_on_workloads(workloads_dir_path):
                 continue
             
             # Start workload
+            workload_file_path = os.path.join(workloads_dir_path, workload)
+            print (workload_file_path)
             Cos_bench_command = subprocess.run(["bash", cosbench_command, submit, workload], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
             if Cos_bench_command.returncode == 1:
                 print("\033[91mStarting workload failed. Skipping this workload.\033[0m")
