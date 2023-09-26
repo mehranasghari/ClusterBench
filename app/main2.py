0b1213b33f9736c70a8aa651ec57207f665a4cc0
alireza-hdri <alirezaheydari617@yahoo.com>
2023-09-26 10:32:52 +0330

    change on main2.py

--- app/main2.py
+++ app/main2.py
@@ -171,79 +171,55 @@ def process_on_workloads(workloads_dir_path):
                 else:
                     print("\033[91mFile Not Found.\033[0m")
                     break
-                    
+        try:
+            # Find start of first main and end of last main
+            with open(result_csv_path, 'r') as csv_file:
+                reader = csv.reader(csv_file)
 
-'''
+                first_main_launching_time = None
+                last_main_completed_time = None
 
-
-            # Remove config.xml file
-            def remove_file_with_retry(file_path, max_retries=2):
-                for retry in range(max_retries + 1):
-                    if os.path.exists(file_path):
-                        try:
-                            result = subprocess.run(['rm', '-rf', file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
-                        except Exception as e:
-                            print(f"\033[91mAn error occurred when removing '{file_path}': {e}\033[0m")
-
-                        if retry < max_retries:
-                            # Sleep for a short duration before retrying
-                            time.sleep(1)
-                        else:
-                            print(f"\033[91mMaximum retries reached ({max_retries}). File removal failed for '{file_path}'\033[0m")
-                    else:
-                        break
-                    print("\033[91mFile Not Found.\033[0m")
-
-            try:
-                # Find start of first main and end of last main
-                with open(result_csv_path, 'r') as csv_file:
-                    reader = csv.reader(csv_file)
-
-                    first_main_launching_time = None
-                    last_main_completed_time = None
-
-                    for row in reader:
-                        if row and row[0].endswith('main'):
-                            if first_main_launching_time is None:
-                                first_main_launching_time = row[21]
+                for row in reader:
+                    if row and row[0].endswith('main'):
+                        if first_main_launching_time is None:
+                            first_main_launching_time = row[21]
                             last_main_completed_time = row[24]
 
-                if first_main_launching_time and last_main_completed_time:
-                    # Write time of workload in time file
-                    time_file_path = os.path.join(result_file_path, 'time')
-                    time_file = open(time_file_path, "w")
-                    start_time = first_main_launching_time.split('@')[1].strip()
-                    end_time = last_main_completed_time.split('@')[1].strip()
-                    start_end_time = start_time + ',' + end_time
-                    time_file.write(start_end_time)
-                    time_file.close()
+            if first_main_launching_time and last_main_completed_time:
+                # Write time of workload in time file
+                time_file_path = os.path.join(result_file_path, 'time')
+                time_file = open(time_file_path, "w")
+                start_time = first_main_launching_time.split('@')[1].strip()
+                end_time = last_main_completed_time.split('@')[1].strip()
+                start_end_time = start_time + ',' + end_time
+                time_file.write(start_end_time)
+                time_file.close()
 
-                    # Start backup phase and its process & get-ring and get-conf to result dir
-                    Ring_address = f"{result_path}/{final_workload_name}/Ring_cluster/"
-                    os.makedirs(Ring_address, exist_ok=True)
+                # Start backup phase and its process & get-ring and get-conf to result dir
+                Ring_address = f"{result_path}/{final_workload_name}/Ring_cluster/"
+                os.makedirs(Ring_address, exist_ok=True)
 
-                    conf_address = f"{result_path}/{final_workload_name}/Config_cluster/"
-                    os.makedirs(conf_address, exist_ok=True)
+                conf_address = f"{result_path}/{final_workload_name}/Config_cluster/"
+                os.makedirs(conf_address, exist_ok=True)
 
-                    get_conf_command = f"python3 ./../Codes/get_conf.py -f {hosts_file_path}"
-                    get_conf_process = subprocess.run(get_conf_command, shell=True)
+                get_conf_command = f"python3 ./../Codes/get_conf.py -f {hosts_file_path}"
+                get_conf_process = subprocess.run(get_conf_command, shell=True)
 
-                    get_ring_command = f"python3 ./../Codes/get_ring.py -f {hosts_file_path}"
-                    get_ring_process = subprocess.run(get_ring_command, shell=True)
+                get_ring_command = f"python3 ./../Codes/get_ring.py -f {hosts_file_path}"
+                get_ring_process = subprocess.run(get_ring_command, shell=True)
 
-                    # Move all *.conf from . to result
-                    ring_mv_command = f"mv *.conf {conf_address}"
-                    ring_mv_process = subprocess.run(ring_mv_command, shell=True)
+                # Move all *.conf from . to result
+                ring_mv_command = f"mv *.conf {conf_address}"
+                ring_mv_process = subprocess.run(ring_mv_command, shell=True)
 
-                    conf_mv_command = f"mv *.txt {Ring_address}"
-                    conf_mv_process = subprocess.run(conf_mv_command, shell=True)
+                conf_mv_command = f"mv *.txt {Ring_address}"
+                conf_mv_process = subprocess.run(conf_mv_command, shell=True)
 
-                    subprocess.call(['python3', backup_script_path, '-t', final_workload_name])
+                subprocess.call(['python3', backup_script_path, '-t', final_workload_name])
 
-            except Exception as e:
-                print(f"\033[91mAn error occurred for workload {workload_name}: {str(e)}\033[0m")
-                continue
-'''
+        except Exception as e:
+            print(f"\033[91mAn error occurred for workload {workload}: {str(e)}\033[0m")
+            continue
 
 # __________________________________________________ #
 '''
