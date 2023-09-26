583b78344ecefb6dd860625ec9671c7234db01f4
alireza-hdri <alirezaheydari617@yahoo.com>
2023-09-26 10:27:26 +0330

    change on main2.py

--- app/main2.py
+++ app/main2.py
@@ -136,7 +136,7 @@ def process_on_workloads(workloads_dir_path):
         archive_csv_path += ".csv"
 
         # Create result csv file
-        result_csv_path = os.path.join(result_file_path, workload_name)
+        result_csv_path = os.path.join(result_file_path, workload)
         result_csv_path += ".csv"
 
         # Copy csv file from archive to result directory
@@ -154,142 +154,27 @@ def process_on_workloads(workloads_dir_path):
             else:
                 print(f"\033[91mMaximum retries reached ({max_retries}). File {archive_csv_path} copy failed.\033[0m")
 
+        # Remove config.xml file
+        def remove_file_with_retry(file_path, max_retries=2):
+            for retry in range(max_retries + 1):
+                if os.path.exists(file_path):
+                    try:
+                        result = subprocess.run(['rm', '-rf', file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
+                    except Exception as e:
+                        print(f"\033[91mAn error occurred when removing '{file_path}': {e}\033[0m")
 
+                    if retry < max_retries:
+                        # Sleep for a short duration before retrying
+                        time.sleep(1)
+                    else:
+                        print(f"\033[91mMaximum retries reached ({max_retries}). File removal failed for '{file_path}'\033[0m")
+                else:
+                    print("\033[91mFile Not Found.\033[0m")
+                    break
+                    
 
 '''
 
-def process_on_workloads(all_xml_path):
-    workloads = os.listdir(all_xml_path)
-    for workload_name in workloads:
-        workload_dir = os.path.join(all_xml_path, workload_name)
-        print()
-        print("Processing on " + workload_dir)
-        for workload_number in range(len(workloads)):
-            # Execute the script before running the test
-            print("\033[1mExecuting pre-test script...\033[0m")
-            time.sleep(1)
-            pre_test_script_failure_num = 0
-            for i in range(max_pre_test_script_failure):
-                pre_test_script = subprocess.run([pre_test_script_path], shell=True)
-                if pre_test_script.returncode == 0:
-                    print("\033[92mPre-test script executed successfully!\033[0m")
-                    break
-                else:
-                    print("\033[91mPre-test script executed with failure!\033[0m")
-                    pre_test_script_failure_num += 1
-                time.sleep(1)
-            if pre_test_script_failure_num == 3:
-                print("\033[91mMaximum pre-test script failures reached. Skipping this workload.\033[0m")
-                continue  # Continue with the next workload if pre-test fails
-
-            # Start workload
-            workload_file_path = os.path.join(all_xml_path, workload_dir, "workload.xml")
-            result = subprocess.run(["bash", cosbench_command, submit, workload_file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-            if result.returncode == 1:
-                print("\033[91mStarting workload failed. Skipping this workload.\033[0m")
-                continue  # Continue with the next workload if workload starting fails
-
-            # Extract ID of workload
-            output_lines = result.stdout.splitlines()
-            print(output_lines)
-            workload_id = ""
-            for line in output_lines:
-                if "ID" in line:
-                    parts = line.split()
-                    if len(parts) > 1:
-                        workload_id = parts[-1]
-                        break
-
-            if not workload_id:
-                print("\033[91mExtracting ID of workload failed.\033[0m")
-                continue  # Continue with the next workload if workload starting fails
-
-            # Generate archive file name of workload
-            archive_file_name = workload_id + "-swift-sample"
-            print(f"Workload Info: ID:{workload_id} Name:{workload_name}")
-            if workload_id == "":
-                print("\033[91mWorkload ID is empty. Skipping this workload.\033[0m")
-                continue
-
-            # Check every second if the workload has ended or not
-            while True:
-                archive_file_path = os.path.join(archive_path, archive_file_name)
-                if os.path.exists(archive_file_path):
-                    break
-                time.sleep(3)  # changed to 3 seconds
-
-            # Create result directory for workloads
-            result_file_path = os.path.join(result_path, workload_name)
-            if os.path.exists(result_file_path):
-                result_file_tail = '_' + '1' + '_'
-                result_file_path += result_file_tail
-                while os.path.exists(result_file_path):
-                    splitted_result_file = result_file_path.split('_')
-                    repeat_number = int(splitted_result_file[-2]) + 1
-                    splitted_result_file[-2] = str(repeat_number)
-                    result_file_path = '_'.join(splitted_result_file)
-
-            os.mkdir(result_file_path)
-            final_workload_name = result_file_path.split('/')[-1]
-
-            # Create and copy workload.log & Add try for copying file up to 2 times
-            archive_log_file = os.path.join(archive_file_path, 'workload.log')
-            result_log_file = os.path.join(result_file_path, 'workload.log')
-            max_retries = 2  # Number of retry attempts
-
-            for retry in range(max_retries + 1):
-                try:
-                    shutil.copy2(archive_log_file, result_log_file)
-                    break  # Exit the loop if copying is successful
-
-                except Exception as e:
-                    print(f"\033[91mAn error occurred: {e}\033[0m")
-
-                if retry < max_retries:
-                    # Sleep for a short duration before retrying
-                    time.sleep(1)
-                else:
-                    print(f"\033[91mMaximum retries reached ({max_retries}). File {archive_log_file} copy failed.\033[0m")
-
-            # Create and copy workload-config.xml
-            archive_config_file = os.path.join(archive_file_path, 'workload-config.xml')
-            result_config_file = os.path.join(result_file_path, 'workload-config.xml')
-            max_retries = 2
-
-            for retry in range(max_retries + 1):
-                try:
-                    shutil.copy2(archive_config_file, result_config_file)
-                    break
-                except Exception as e:
-                    print(f"\033[91mAn error occurred: {e}\033[0m")
-
-                if retry < max_retries:
-                    time.sleep(3)
-                else:
-                    print(f"\033[91mMaximum retries reached ({max_retries}). File {archive_config_file} copy failed.\033[0m")
-
-            # Create archive csv file
-            archive_csv_path = os.path.join(archive_file_path, archive_file_name)
-            archive_csv_path += ".csv"
-
-            # Create result csv file
-            result_csv_path = os.path.join(result_file_path, workload_name)
-            result_csv_path += ".csv"
-
-            # Copy csv file from archive to result directory
-            max_retries = 2
-
-            for retry in range(max_retries + 1):
-                try:
-                    shutil.copy2(archive_csv_path, result_csv_path)
-                    break
-                except Exception as e:
-                    print(f"\033[91mAn error occurred: {e}\033[0m")
-
-                if retry < max_retries:
-                    time.sleep(3)
-                else:
-                    print(f"\033[91mMaximum retries reached ({max_retries}). File {archive_csv_path} copy failed.\033[0m")
 
             # Remove config.xml file
             def remove_file_with_retry(file_path, max_retries=2):
