aa038d110ac389b97b040c38752e63687ed4045d
alireza-hdri <alirezaheydari617@yahoo.com>
2023-10-04 07:13:26 +0330

    reneder and its config updatedf

---
+++ Codes/renderer.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+import json
+
+grafana_config_file_path = "./../conf/Software/GrafanaConfig.json"
+
+# import data from json file
+with open (grafana_config_file_path, 'r') as file:
+    grafana_config_data = json.load(file)
+key = grafana_config_data["api_key"]
+address = grafana_config_data["URL"]
+port = grafana_config_data["port"]
+uid = grafana_config_data["uid"]
+dashboard_name = grafana_config_data["dashboard_name"]
+start_time = "1696304493550"
+end_time = "1696390893550"
+
+curl_command = f"curl -o output.png -H \"Authorization: Bearer {key}\" \"http://{address}:{port}/render/d-solo/{uid}/{dashboard_name}?orgId=1&from={start_time}&to={end_time}1&panelId=39&width=1000&height=500&tz=Asia%2FTehran\""
+curl_process = subprocess.run(curl_command, shell=True)
+curl_exit_code = curl_process.returncode
+if curl_exit_code == 0:
+    print("Success")
+else:
+    print("Error")
