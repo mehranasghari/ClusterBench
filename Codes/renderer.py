ad8243bb2f35fa3d05e48ce2442aacd714295218
alireza-hdri <alirezaheydari617@yahoo.com>
2023-10-04 10:39:05 +0330

    renderer code updated

--- Codes/renderer.py
+++ Codes/renderer.py
@@ -15,19 +15,19 @@ dashboard_name = grafana_config_data["dashboard_name"]
 org_id = grafana_config_data["org_id"]
 timeVariable = grafana_config_data["timeVariable"]
 DataSource = grafana_config_data["DataSource"]
-start_time = "1696304493550" #should use backsup start time in utc and timestamp
-end_time = "1696390893550"   #should use backsup start time in utc and timestamp
+start_time = "1696399144231" #should use backsup start time in utc and timestamp
+end_time = "1696400944231"   #should use backsup start time in utc and timestamp
 panel_id = "39"
 
 # Start renderring
 def renderer(address, port, uid, dashboard_name, org_id, timeVariable, DataSource, start_time, end_time, panel_id):
     
-    curl_command = f"curl -o output.png -H \"Authorization: Bearer {key}\" http://{address}:{port}/render/d-solo/{uid}/{dashboard_name}?orgId={org_id}&var-hostIs=m-r1z1s5-controller&var-timeVariable={timeVariable}&var-DataSource={DataSource}&from=1696399144231&to=1696400944231&panelId={panel_id}&width=1000&height=500&tz=Asia%2FTehran\""
+    curl_command = f"curl -o output.png -H \"Authorization: Bearer {key}\" http://{address}:{port}/render/d-solo/{uid}/{dashboard_name}?orgId={org_id}&var-hostIs=m-r1z1s5-controller&var-timeVariable={timeVariable}&var-DataSource={DataSource}&from={start_time}&to={end_time}&panelId={panel_id}&width=1000&height=500&tz=Asia%2FTehran\""
     curl_process = subprocess.run(curl_command, shell=True)
     curl_exit_code = curl_process.returncode
     if curl_exit_code == 0:
         print("Success")
     else:
         print("Error")
-        
+
 renderer(address, port, uid, dashboard_name, org_id, timeVariable, DataSource, start_time, end_time, panel_id)
