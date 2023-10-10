b61764a7935cc772d371885a0428e83a165987e4
alireza-hdri <alirezaheydari617@yahoo.com>
2023-10-10 17:41:35 +0330

    change on renderer.py

--- Codes/renderer.py
+++ Codes/renderer.py
@@ -57,10 +57,10 @@ save_path = args.path if args.path else "./Pictures"
 try:
     delete_process = subprocess.run(f"rm -rf {save_path}/*", shell=True)
     if delete_process.returncode != 0:
-        print(f"Error in deleting {save_path}")
+        print(f"\033[91mError in deleting {save_path}\033[0m")
     create_process = subprocess.run(f"mkdir -p {save_path}", shell=True)
     if create_process.returncode != 0:
-        print(f"Error in creating {save_path}")
+        print(f"\033[91mError in creating {save_path}\033[0m")
 except:
     exit()
 
@@ -90,14 +90,15 @@ def renderer(address, port, uid, dashboard_name, org_id, timeVariable, DataSourc
                     pix_curl_command = f"""curl -s -o {save_path}/{host}-{panel_names[0+i]}.png -H "Authorization: Bearer {key}" 'http://{address}:{port}/render/d-solo/{uid}/{dashboard_name}?orgId={org_id}&var-hostIs={host}&var-timeVariable={timeVariable}&var-DataSource={DataSource}&from={start_timestamp}&to={end_timestamp}&panelId={panel_id}&width={width}&height={height}&tz={tz}'"""
                     pix_curl_process = subprocess.run(pix_curl_command, shell=True, stdout=subprocess.PIPE)
                     curl_exit_code = pix_curl_process.returncode
+                    print(pix_curl_command)
                     if curl_exit_code == 0:
-                        print("Success")
+                        print("\033[92mSuccess\033[0m")
                         i += 1
                     else:
                         print(pix_curl_process.stdout)
             else:
-                print("Error, there is no host in the list")
+                print("\033[91mError, there is no host in the list\033[0m")
     except Exception as e:
-        print("Error, in try part")
+        print("\033[91mError, in try part\033[0m")
         print(e)
 renderer(address, port, uid, dashboard_name, org_id, timeVariable, DataSource, start_timestamp, end_timestamp, width, height, all_hosts, save_path, tz)
